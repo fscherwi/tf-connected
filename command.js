b17cd07684a774825308e912d97a8b7887d8c74c
fscherwi <Florian@ScherwitzlsiMac.home>
2015-08-11 15:57:45 +0200

    alphabetically ordered Bricklets

--- command.js
+++ command.js
@@ -81,12 +81,20 @@ if (!program.args.length) {
       }
 
       // BRICKLET'S
-      else if (deviceIdentifier === Tinkerforge.BrickletAmbientLight.DEVICE_IDENTIFIER) {
+      else if (deviceIdentifier === Tinkerforge.BrickletAccelerometer.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletAccelerometer.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletAmbientLight.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletAmbientLight.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletAmbientLightV2.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletAmbientLightV2.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletAnalogIn.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletAnalogIn.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletAnalogInV2.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletAnalogInV2.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletAnalogOut.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletAnalogOut.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletAnalogOutV2.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletAnalogOutV2.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletBarometer.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletBarometer.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletColor.DEVICE_IDENTIFIER) {
@@ -103,12 +111,18 @@ if (!program.args.length) {
         console.log('NAME: ' + Tinkerforge.BrickletDualButton.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletDualRelay.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletDualRelay.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletDustDetector.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletDustDetector.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletGPS.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletGPS.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletHallEffect.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletHallEffect.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletHumidity.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletHumidity.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialAnalogIn.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletIndustrialAnalogIn.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialDigitalOut4.DEVICE_IDENTIFIER) {
@@ -123,6 +137,8 @@ if (!program.args.length) {
         console.log('NAME: ' + Tinkerforge.BrickletIO4.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletJoystick.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletJoystick.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletLaserRangeFinder.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletLaserRangeFinder.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletLCD16x2.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletLCD16x2.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletLCD20x4.DEVICE_IDENTIFIER) {
@@ -133,6 +149,8 @@ if (!program.args.length) {
         console.log('NAME: ' + Tinkerforge.BrickletLine.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletLinearPoti.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletLinearPoti.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletLoadCell.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletLoadCell.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletMoisture.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletMoisture.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletMotionDetector.DEVICE_IDENTIFIER) {
@@ -153,6 +171,8 @@ if (!program.args.length) {
         console.log('NAME: ' + Tinkerforge.BrickletRotaryEncoder.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletRotaryPoti.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletRotaryPoti.DEVICE_DISPLAY_NAME);
+      } else if (deviceIdentifier === Tinkerforge.BrickletRS232.DEVICE_IDENTIFIER) {
+        console.log('NAME: ' + Tinkerforge.BrickletRS232.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletSolidStateRelay.DEVICE_IDENTIFIER) {
@@ -169,28 +189,6 @@ if (!program.args.length) {
         console.log('NAME: ' + Tinkerforge.BrickletVoltage.DEVICE_DISPLAY_NAME);
       } else if (deviceIdentifier === Tinkerforge.BrickletVoltageCurrent.DEVICE_IDENTIFIER) {
         console.log('NAME: ' + Tinkerforge.BrickletVoltageCurrent.DEVICE_DISPLAY_NAME);
-      }
-      // new Bricklets
-      else if (deviceIdentifier === Tinkerforge.BrickletAccelerometer.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletAccelerometer.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletAmbientLightV2.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletAmbientLightV2.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletAnalogInV2.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletAnalogInV2.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletAnalogOutV2.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletAnalogOutV2.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletDustDetector.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletDustDetector.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletIndustrialAnalogIn.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletIndustrialAnalogIn.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletLaserRangeFinder.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletLaserRangeFinder.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletLoadCell.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletLoadCell.DEVICE_DISPLAY_NAME);
-      } else if (deviceIdentifier === Tinkerforge.BrickletRS232.DEVICE_IDENTIFIER) {
-        console.log('NAME: ' + Tinkerforge.BrickletRS232.DEVICE_DISPLAY_NAME);
       } else {
         console.log('NAME: unknown');
       }
