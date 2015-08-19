5fa338c896fe0a3ba9e6ed963751b7afc702bf95
fscherwi <Florian@ScherwitzlsiMac.home>
2015-08-19 19:41:00 +0200

    fix

---
+++ command.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+  .version(require('./package.json').version)
+  .usage('[options] list')
+  .option('-a, --advanced', 'Shows advanced information')
+  .option('-h, --host [host]', 'The HOST, default to "localhost"')
+  .option('-p, --port [port]', 'The PORT, default to "4223"', parseInt)
+  .parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+} else {
+  require('./connected.js');
+}
