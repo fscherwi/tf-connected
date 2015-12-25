addbbb9f3535ab935072f5b150b19c1e3f286a45
fscherwi <Florian@ScherwitzlsiMac.home>
2015-12-25 11:23:55 +0100

    new get algorithm

--- get_name.js
+++ get_name.js
@@ -1,125 +1,129 @@
 var Tinkerforge = require('tinkerforge');
 /* istanbul ignore next */
 exports.get = function get(deviceIdentifier) {
-  switch (deviceIdentifier) {
-    case Tinkerforge.BrickDC.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickDC.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickIMU.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickIMU.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickMaster.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickMaster.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickServo.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickServo.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickStepper.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickStepper.DEVICE_DISPLAY_NAME;
-      // BRICKLET'S
-    case Tinkerforge.BrickletAccelerometer.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAccelerometer.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAmbientLight.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAmbientLight.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAmbientLightV2.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAmbientLightV2.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAnalogIn.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAnalogIn.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAnalogInV2.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAnalogInV2.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAnalogOut.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAnalogOut.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletAnalogOutV2.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletAnalogOutV2.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletBarometer.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletBarometer.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletColor.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletColor.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletCurrent12.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletCurrent12.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletCurrent25.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletCurrent25.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletDistanceIR.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletDistanceIR.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletDistanceUS.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletDistanceUS.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletDualButton.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletDualButton.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletDualRelay.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletDualRelay.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletDustDetector.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletDustDetector.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletGPS.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletGPS.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletHallEffect.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletHallEffect.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletHumidity.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletHumidity.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIndustrialDigitalOut4.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIndustrialDigitalOut4.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIndustrialDual020mA.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIndustrialDual020mA.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIndustrialQuadRelay.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIndustrialQuadRelay.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIO16.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIO16.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletIO4.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletIO4.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletJoystick.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletJoystick.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLaserRangeFinder.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLaserRangeFinder.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLCD16x2.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLCD16x2.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLCD20x4.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLCD20x4.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLEDStrip.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLEDStrip.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLine.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLine.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLinearPoti.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLinearPoti.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletLoadCell.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletLoadCell.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletMoisture.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletMoisture.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletMotionDetector.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletMotionDetector.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletMultiTouch.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletMultiTouch.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletNFCRFID.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletNFCRFID.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletPiezoBuzzer.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletPiezoBuzzer.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletPiezoSpeaker.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletPiezoSpeaker.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletPTC.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletPTC.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletRemoteSwitch.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletRemoteSwitch.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletRotaryEncoder.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletRotaryEncoder.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletRotaryPoti.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletRotaryPoti.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletRS232.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletRS232.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletSolidStateRelay.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletSolidStateRelay.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletSoundIntensity.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletSoundIntensity.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletTemperature.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletTemperature.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletTemperatureIR.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletTemperatureIR.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletTilt.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletTilt.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletVoltage.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletVoltage.DEVICE_DISPLAY_NAME;
-    case Tinkerforge.BrickletVoltageCurrent.DEVICE_IDENTIFIER:
-      return Tinkerforge.BrickletVoltageCurrent.DEVICE_DISPLAY_NAME;
-    default:
-      return 'unknown';
+  var names = [
+    Tinkerforge.BrickDC.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickDC.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickIMU.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickIMU.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickMaster.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickMaster.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickServo.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickServo.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickStepper.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickStepper.DEVICE_DISPLAY_NAME,
+    // BRICKLET'S
+    Tinkerforge.BrickletAccelerometer.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAccelerometer.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAmbientLight.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAmbientLight.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAmbientLightV2.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAmbientLightV2.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAnalogIn.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAnalogIn.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAnalogInV2.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAnalogInV2.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAnalogOut.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAnalogOut.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletAnalogOutV2.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletAnalogOutV2.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletBarometer.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletBarometer.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletColor.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletColor.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletCurrent12.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletCurrent12.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletCurrent25.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletCurrent25.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletDistanceIR.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletDistanceIR.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletDistanceUS.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletDistanceUS.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletDualButton.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletDualButton.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletDualRelay.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletDualRelay.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletDustDetector.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletDustDetector.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletGPS.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletGPS.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletHallEffect.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletHallEffect.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletHumidity.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletHumidity.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIndustrialAnalogOut.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIndustrialDigitalIn4.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIndustrialDigitalOut4.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIndustrialDigitalOut4.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIndustrialDual020mA.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIndustrialDual020mA.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIndustrialQuadRelay.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIndustrialQuadRelay.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIO16.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIO16.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletIO4.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletIO4.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletJoystick.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletJoystick.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLaserRangeFinder.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLaserRangeFinder.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLCD16x2.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLCD16x2.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLCD20x4.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLCD20x4.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLEDStrip.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLEDStrip.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLine.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLine.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLinearPoti.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLinearPoti.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletLoadCell.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletLoadCell.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletMoisture.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletMoisture.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletMotionDetector.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletMotionDetector.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletMultiTouch.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletMultiTouch.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletNFCRFID.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletNFCRFID.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletPiezoBuzzer.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletPiezoBuzzer.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletPiezoSpeaker.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletPiezoSpeaker.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletPTC.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletPTC.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletRemoteSwitch.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletRemoteSwitch.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletRotaryEncoder.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletRotaryEncoder.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletRotaryPoti.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletRotaryPoti.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletRS232.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletRS232.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletSegmentDisplay4x7.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletSolidStateRelay.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletSolidStateRelay.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletSoundIntensity.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletSoundIntensity.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletTemperature.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletTemperature.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletTemperatureIR.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletTemperatureIR.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletTilt.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletTilt.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletVoltage.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletVoltage.DEVICE_DISPLAY_NAME,
+    Tinkerforge.BrickletVoltageCurrent.DEVICE_IDENTIFIER,
+    Tinkerforge.BrickletVoltageCurrent.DEVICE_DISPLAY_NAME
+  ];
+  var length = names.length;
+  for (var i = 0; i < length; i=i+2) {
+    if (deviceIdentifier == names[i]) {
+      return names[i+1];
+    }
   }
 };
