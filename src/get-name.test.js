476806d244ee74f0a9b985a91595fac6d60bc127
fscherwi <fscherwi@outlook.com>
2019-10-12 23:03:52 +0200

    add new bricklets

--- src/get-name.test.js
+++ src/get-name.test.js
@@ -4,15 +4,15 @@ const Tinkerforge = require('tinkerforge');
 const {name} = require('./get-name.js');
 
 test('Brick Master', t => {
-	t.is(name(13), Tinkerforge.BrickMaster.DEVICE_DISPLAY_NAME);
+	t.is(name(Tinkerforge.BrickMaster.DEVICE_IDENTIFIER), Tinkerforge.BrickMaster.DEVICE_DISPLAY_NAME);
 });
 
 test('Brick RED', t => {
-	t.is(name(17), Tinkerforge.BrickRED.DEVICE_DISPLAY_NAME);
+	t.is(name(Tinkerforge.BrickRED.DEVICE_IDENTIFIER), Tinkerforge.BrickRED.DEVICE_DISPLAY_NAME);
 });
 
 test('Bricklet Accelerometer V2', t => {
-	t.is(name(2130), Tinkerforge.BrickletAccelerometerV2.DEVICE_DISPLAY_NAME);
+	t.is(name(Tinkerforge.BrickletAccelerometerV2.DEVICE_IDENTIFIER), Tinkerforge.BrickletAccelerometerV2.DEVICE_DISPLAY_NAME);
 });
 
 test('unknown Brick/Bricklet name', t => {
